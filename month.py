27c7690415152ab120daf2875ad227066369f16d
Kurt Lyell <kurtly@nmsu.edu>
2024-10-11 16:12:10 -0600

    inital commit

---
+++ month.py
@@ -0,0 +1,10 @@
+import init_connect
+def get_month_stats(month):
+    if month == 'jan':
+        month = '01'
+    elif month == 'feb':
+        month = '02'
+    elif month == 'mar':
+        month = '03'
+
+
